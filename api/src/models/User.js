dc7190ae832641f9d1c92de03bba85007501f68c
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-21 12:18:08 -0300

    user model ok

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -9,18 +9,27 @@ module.exports = (sequelize) => {
         },
         username: {
             type: DataTypes.STRING(20),
+            unique: true,
             allowNull: false,
+            validate: {
+                isLowercase: true,
+            },
         },
         email: {
             type: DataTypes.STRING,
+            unique: true,
             allowNull: false,
             validate: {
-                isUrl: true,
+                isEmail: true,
             },
         },
         password: {
-            type: DataTypes.STRING(20),
+            type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                is: /^(?=.*\d)(?=.*[A-Z])(?!.*\s).{6,15}$/,
+            },
+            // Entre 6 y 15 digitos, al menos una mayuscula y un numero y que no contenga espacios en blanco
         },
     });
 };
