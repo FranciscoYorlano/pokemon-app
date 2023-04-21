6692b4ce46e5615e1a977d6c0899d5faf3ee5ce2
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-21 18:56:34 -0300

    sign in, sign up pages

---
+++ client/src/pages/signin/Signin.jsx
@@ -0,0 +1,12 @@
+// ======================== Styles
+import styles from "./signin.module.css";
+import logo from "../../assets/logo.png";
+
+// ======================== React Router
+import { Link } from "react-router-dom";
+
+const Signin = () => {
+    return <div>Signin</div>;
+};
+
+export default Signin;
---
+++ client/src/pages/signup/Signup.jsx
@@ -0,0 +1,12 @@
+// ======================== Styles
+import styles from "./signup.module.css";
+import logo from "../../assets/logo.png";
+
+// ======================== React Router
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+    return <div>Signup</div>;
+};
+
+export default Signup;
