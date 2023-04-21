07b3ecb4ddca163371682bab57d60a502cc028fa
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-21 11:34:35 -0300

    new models

--- api/src/db.js
+++ api/src/db.js
@@ -4,12 +4,13 @@ const { Sequelize } = require("sequelize");
 // ======================== Models requires
 const pokemonDefiner = require("./models/Pokemon");
 const typeDefiner = require("./models/Type");
+const userDefiner = require("./models/User");
 
 // ======================== Sequelize
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
     {
         logging: false,
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -19,11 +20,15 @@ const sequelize = new Sequelize(
 // ======================== Models definers
 pokemonDefiner(sequelize);
 typeDefiner(sequelize);
-const { Pokemon, Type, PokemonTypes } = sequelize.models;
+userDefiner(sequelize);
+const { Pokemon, Type, User } = sequelize.models;
 
 // ======================== Models relations
-Pokemon.belongsToMany(Type, { through: "PokemonsTypes" });
 Type.belongsToMany(Pokemon, { through: "PokemonsTypes" });
+Pokemon.belongsToMany(Type, { through: "PokemonsTypes" });
+
+Pokemon.belongsToMany(User, { through: "UsersPokemons" });
+User.belongsToMany(Pokemon, { through: "UsersPokemons" });
 
 module.exports = {
     ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
