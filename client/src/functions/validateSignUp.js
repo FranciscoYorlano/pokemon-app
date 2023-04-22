1b18b0ee839eea2d2a3e7e15f9e06ff650cd7425
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-22 13:27:03 -0300

    validate improve

--- client/src/functions/validateSignUp.js
+++ client/src/functions/validateSignUp.js
@@ -2,8 +2,8 @@ const REGEX_USERNAME = /^[a-z0-9_]{3,20}$/;
 const REGEX_EMAIL = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const REGEX_PASSWORD = /^(?=.*\d)(?=.*[A-Z])(?!.*\s).{6,15}$/;
 
-export const validateSignUp = (userData) => {
-    const { email, username, password } = userData;
+export const validateSignUp = (userData, users) => {
+    const { email, username, password, passwordConfirm } = userData;
 
     const errors = {
         email: "",
@@ -17,6 +17,10 @@ export const validateSignUp = (userData) => {
         if (!REGEX_USERNAME.test(username)) {
             errors.username =
                 "Username must be between 3 and 20 characters long, and can only contain lowercase letters, numbers and underscores.";
+        } else {
+            if (users.some((user) => user.username === username)) {
+                errors.username = "Username is already in use.";
+            }
         }
     }
 
@@ -25,6 +29,10 @@ export const validateSignUp = (userData) => {
     } else {
         if (!REGEX_EMAIL.test(email)) {
             errors.email = "Email is invalid.";
+        } else {
+            if (users.some((user) => user.email === email)) {
+                errors.email = "Email is already in use.";
+            }
         }
     }
 
@@ -34,6 +42,12 @@ export const validateSignUp = (userData) => {
         if (!REGEX_PASSWORD.test(password)) {
             errors.password =
                 "Password must be between 6 and 15 characters long, and contain at least one uppercase letter, one number, and no spaces.";
+        }
+    }
+
+    if ((passwordConfirm || password) && !errors.password) {
+        if (password !== passwordConfirm) {
+            errors.password = "Passwords do not match";
         }
     }
 
