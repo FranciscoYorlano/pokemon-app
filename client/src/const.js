c45bc7edcfb1faebd2d517eafa2bc03d44e01a41
Francisco Yorlano <francisco.yorlano.arg@gmail.com>
2023-04-24 08:12:23 -0300

    consts

---
+++ client/src/const.js
@@ -0,0 +1,17 @@
+export const FILTERS = {
+    BY_TYPE: {
+        ALL_TYPES: "allTypes",
+    },
+    BY_SOURCE: {
+        DATABASE: "database",
+        POKEAPI: "pokeapi",
+    },
+};
+
+export const SORT = {
+    DEFAULT: "default",
+    ALPHABETICAL_ASC: "alphabeticalAsc",
+    ALPHABETICAL_DESC: "alphabeticalDesc",
+    ATTACK_ASC: "attackAsc",
+    ATTACK_DESC: "attackDesc",
+};
